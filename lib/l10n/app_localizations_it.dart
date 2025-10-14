ac2f5209940078128f0ffdb5bf365cdde682b10c
Marco Acorte <macorte@gmail.com>
2025-10-14 11:47:01 +0200

    add Italian localization support and translations

---
+++ lib/l10n/app_localizations_it.dart
@@ -0,0 +1,208 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Italian (`it`).
+class AppLocalizationsIt extends AppLocalizations {
+  AppLocalizationsIt([String locale = 'it']) : super(locale);
+
+  @override
+  String get no => 'No';
+
+  @override
+  String get yes => 'Sì';
+
+  @override
+  String get mainNoCares => 'Evviva! Non hai piante in attesa di cure';
+
+  @override
+  String get mainNoPlants => 'Il giardino è vuoto, piantiamo qualcosa?';
+
+  @override
+  String get buttonGarden => 'Giardino';
+
+  @override
+  String get buttonToday => 'Oggi';
+
+  @override
+  String get tooltipCareAll => 'Applica cure a tutte le piante';
+
+  @override
+  String get tooltipShowCalendar => 'Mostra Calendario';
+
+  @override
+  String get tooltipSettings => 'Impostazioni';
+
+  @override
+  String get tooltipNewPlant => 'Aggiungi nuova pianta';
+
+  @override
+  String get selectDays => 'Seleziona giorni';
+
+  @override
+  String get ok => 'OK';
+
+  @override
+  String get every => 'ogni';
+
+  @override
+  String get days => 'giorni';
+
+  @override
+  String get never => 'Mai';
+
+  @override
+  String get titleEditPlant => 'Modifica';
+
+  @override
+  String get titleNewPlant => 'Nuovo';
+
+  @override
+  String get tooltipCameraImage => 'Scatta foto';
+
+  @override
+  String get tooltipNextAvatar => 'Avatar successivo';
+
+  @override
+  String get tooltipGalleryImage => 'Scegli immagine dalla galleria';
+
+  @override
+  String get emptyError => 'Inserisci del testo';
+
+  @override
+  String get conflictError => 'Il nome della pianta esiste già';
+
+  @override
+  String get labelName => 'Nome';
+
+  @override
+  String get exampleName => 'Es: Pilea';
+
+  @override
+  String get labelDescription => 'Descrizione';
+
+  @override
+  String get labelLocation => 'Posizione';
+
+  @override
+  String get exampleLocation => 'Es: Cortile';
+
+  @override
+  String get labelDayPlanted => 'Giorno di piantagione';
+
+  @override
+  String get saveButton => 'Salva';
+
+  @override
+  String get careAll => 'Hai curato tutte le piante?';
+
+  @override
+  String get careAllBody =>
+      'Questa azione segnerà tutte le piante come curate per il ciclo odierno.';
+
+  @override
+  String get water => 'Innaffia';
+
+  @override
+  String get spray => 'Nebulizza';
+
+  @override
+  String get rotate => 'Ruota';
+
+  @override
+  String get prune => 'Pota';
+
+  @override
+  String get fertilise => 'Fertilizza';
+
+  @override
+  String get transplant => 'Trapianta';
+
+  @override
+  String get clean => 'Pulisci';
+
+  @override
+  String get now => 'Oggi';
+
+  @override
+  String get daysLeft => 'giorni rimanenti';
+
+  @override
+  String get daysLate => 'Da';
+
+  @override
+  String get tooltipEdit => 'Modifica questa pianta';
+
+  @override
+  String get deleteButton => 'Elimina';
+
+  @override
+  String get noCaresError => 'Seleziona almeno una cura';
+
+  @override
+  String get careButton => 'Cura';
+
+  @override
+  String get selectHours => 'Seleziona ore';
+
+  @override
+  String get notifyEvery => 'Notifica ogni';
+
+  @override
+  String get hours => 'ore';
+
+  @override
+  String get testNotificationButton => 'Testa notifica';
+
+  @override
+  String get testNotificationTitle => 'Notifica di Test Florae';
+
+  @override
+  String get testNotificationBody => 'Questo è un messaggio di test';
+
+  @override
+  String get aboutFloraeButton => 'Informazioni su Florae';
+
+  @override
+  String get notificationInfo =>
+      'L\'orario delle notifiche verrà reimpostato quando entri nell\'App.\n\nTieni presente che alcuni dispositivi eseguono ottimizzazioni della batteria molto aggressive che potrebbero impedire il corretto invio delle notifiche.';
+
+  @override
+  String get careNotificationTitle => 'Le piante richiedono cure';
+
+  @override
+  String get careNotificationName => 'Promemoria cure';
+
+  @override
+  String get careNotificationDescription => 'Ricevi notifiche per le cure delle piante';
+
+  @override
+  String get deletePlantTitle => 'Elimina pianta';
+
+  @override
+  String get deletePlantBody =>
+      'Stai per eliminare definitivamente la tua pianta, questa azione non può essere annullata.';
+
+  @override
+  String get exportData => 'Esporta giardino';
+
+  @override
+  String get importData => 'Importa giardino';
+
+  @override
+  String get unsuccessfullyRestore => 'Impossibile ripristinare il backup.';
+
+  @override
+  String get unsuccessfullyBackup => 'Impossibile creare un file di backup.';
+
+  @override
+  String get atDay => 'Durante il giorno';
+
+  @override
+  String get atNoon => 'A mezzogiorno';
+
+  @override
+  String get atNight => 'Di notte';
+}
